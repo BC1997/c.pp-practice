abf2eb27803b3c7f2e104366274d57890fc31b10
Brandon Carreon <brandon.carreon@g.austincc.edu>
2017-01-02 12:09:26 -0600

    Create variablepractice.cpp

---
+++ variablepractice.cpp
@@ -0,0 +1,16 @@
+//This program has a variable
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number;
+  
+  number = 5
+  cout << "The value of number is " << number << endl;
+  
+  number = 7;
+  cout << "The value of number is now " << number << endl;
+  
+  return 0;
+ }
